c53f67bb606a9e99fc97acf378addd221ac25227
Elena Zlateva <elena.zlateva84@gmail.com>
2019-08-21 23:15:26 +0300

    updating cv
    added new projects

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,6 @@ function printIcon() {
 
 
 let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
