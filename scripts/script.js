dc36491319bb8572a71e6f0943c2b9b79ce58cec
Elena Zlateva <elena.zlateva84@gmail.com>
2019-08-22 11:17:38 +0300

    swiper slider integration

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,16 @@ function printIcon() {
     window.print();
 }
 
+$(document).ready(function() {
 
-let slideIndex = 1;
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
-    if (n == undefined) { n = ++slideIndex }
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+    //slider
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        autoplay:2500,
+        speed: 3000,
+        loop: true,
+    });
+});
